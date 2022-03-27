539ea165044bee24ff8fc6796634431dcdf49bc4
dlrjs2360 <lhg3670@naver.com>
2022-03-28 03:06:18 +0900

    css static

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 import express from "express"
 import path from "path"
-import route from "./src/routes.js"
+import router from "./src/routes.js"
 
 const app = express()
 const __dirname = path.resolve()
 
-app.set("views", path.join(__dirname, "./views"))
-app.set("view engine", "ejs")
-
-app.use(route)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "src", "views"))
+
+app.use(router)
 
 app.listen(8000, () => {
   console.log("Access through http://localhost:8000")
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,21 @@
 import express from "express"
-const route = express.Router()
 
-route.get("/", (req, res) => {
+const router = express.Router()
+
+router.get("/", (req, res) => {
   res.render("index")
 })
 
-export default route
+router.get("/button", (req, res) => {
+  res.render("button")
+})
+
+router.post("/inform", (req, res) => {
+  const { item1, item2, item3, item4 } = req.body
+  console.log(
+    `item1 : ${item1} item2 : ${item2} item3 : ${item3} item4 : ${item4}`
+  )
+  res.redirect("/")
+})
+
+export default router
