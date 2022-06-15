147a35b5c3e2c195d396d6e50c15b16f5766df80
dlrjs2360 <lhg3670@naver.com>
2022-06-15 23:32:16 +0900

    32323

--- src/controller/elec.js
+++ src/controller/elec.js
@@ -51,13 +51,25 @@ export async function newInput(req, res) {
   const temp = Fee.split("원")[0].replace(",", "")
 
   Fee = parseInt(temp)
-
+  Year = parseInt(Year)
+  Month = parseInt(Month)
   var options = {
     mode: "text",
     pythonOptions: ["-u"],
     args: [Year, Month, See, Goo, Fee],
   }
-  if (Year == Pyear && Month == Pmonth) {
+
+  console.log("파이썬으로 넘어가는 값", Year, Month, See, Goo, Fee)
+  console.log(Pyear, Pmonth)
+  console.log(parseInt(Year), parseInt(Month))
+  console.time("연산시간")
+
+  if (
+    Year == Pyear &&
+    (parseInt(Month) == Pmonth + 1 ||
+      parseInt(Month) == Pmonth ||
+      parseInt(Month) == Pmonth - 1)
+  ) {
     new Elec({
       Year,
       Month,
@@ -66,29 +78,23 @@ export async function newInput(req, res) {
       Fee,
     }).save()
     console.log("DB에 저장")
-  }
+  } else {
+    PythonShell.run("hhh.py", options, function (err, result) {
+      if (err) throw err
+      console.log(result)
+      const data_split = result[0].split(" ")
+      console.log("파이썬으로부터 받은 값", data_split)
 
-  console.log("파이썬으로 넘어가는 값", Year, Month, See, Goo, Fee)
-  console.log(Pyear, Pmonth)
-  console.time("연산시간")
-
-  if (Year == Pyear && Month == Pmonth) {
-    console.log("DB에서 값 받아오기")
+      data_split[3] = `/images/${data_split[3]}.png`
+      data_split[4] = `/rank/${data_split[2]}.png`
+      console.timeEnd("연산시간")
+      console.log(
+        "================================================================="
+      )
+      res.render("result", { data: data_split })
+    })
   }
+  // child-process로 실행방법
   // const fromPy = spawn2("python", ["hhh.py", Year, Month, See, Goo, Fee])
   // fromPy.stdout.on("data", function (data) {
-  PythonShell.run("hhh.py", options, function (err, result) {
-    if (err) throw err
-    console.log(result)
-    const data_split = result[0].split(" ")
-    console.log("파이썬으로부터 받은 값", data_split)
-
-    data_split[3] = `/tmp/${data_split[3]}.png`
-    data_split[4] = `/rank/${data_split[2]}.png`
-    console.timeEnd("연산시간 ")
-    console.log(
-      "================================================================="
-    )
-    res.render("result", { data: data_split })
-  })
 }
